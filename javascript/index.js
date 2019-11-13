bd94c62deaf7dafa25c5ab2e969fdfad55e8649e
Ajschlander <Aschlander21@gmail.com>
2019-11-13 14:22:41 -0600

    Added logic for every checkbox combination

--- javascript/index.js
+++ javascript/index.js
@@ -6,6 +6,14 @@ const noLowercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*_";
 const noUppercaseCharacters = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*_";
 const noSpecialNumericalCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 const noSpecialLowercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const noSpecialUppercaseCharacters = "abcdefghijklmnopqrstuvwxyz0123456789";
+const noNumericalLowercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*_";
+const noNumericalUppercaseCharacters = "abcdefghijklmnopqrstuvwxyz!@#$%^&*_";
+const noLowercaseUppercaseCharacters = "0123456789!@#$%^&*_";
+const justSpecialCharacters = "!@#$%^&*_";
+const justNumericalCharacters = "0123456789";
+const justLowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+const justUppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 // Add event listener to generate button
 document.getElementById('generate').addEventListener("click", function() {
@@ -119,6 +127,118 @@ document.getElementById('generate').addEventListener("click", function() {
        let charactersLength = noSpecialLowercaseCharacters.length;
        for ( let i = 0; i < length; i++ ) {
           password += noSpecialLowercaseCharacters.charAt(Math.floor(Math.random() * charactersLength));
+       }
+      return password;
+    }
+
+    console.log(makePassword(length));
+  }
+
+  // Generate password without special or uppercase characters
+  else if (special == false && numerical == true && lowercase == true && uppercase == false) {
+    function makePassword(length) {
+       let password           = '';
+       let charactersLength = noSpecialUppercaseCharacters.length;
+       for ( let i = 0; i < length; i++ ) {
+          password += noSpecialUppercaseCharacters.charAt(Math.floor(Math.random() * charactersLength));
+       }
+      return password;
+    }
+
+    console.log(makePassword(length));
+  }
+
+  // Generate password without numerical or lowercase characters
+  else if (special == true && numerical == false && lowercase == false && uppercase == true) {
+    function makePassword(length) {
+       let password           = '';
+       let charactersLength = noNumericalLowercaseCharacters.length;
+       for ( let i = 0; i < length; i++ ) {
+          password += noNumericalLowercaseCharacters.charAt(Math.floor(Math.random() * charactersLength));
+       }
+      return password;
+    }
+
+    console.log(makePassword(length));
+  }
+
+  // Generate password without numerical or uppercase characters
+  else if (special == true && numerical == false && lowercase == true && uppercase == false) {
+    function makePassword(length) {
+       let password           = '';
+       let charactersLength = noNumericalUppercaseCharacters.length;
+       for ( let i = 0; i < length; i++ ) {
+          password += noNumericalUppercaseCharacters.charAt(Math.floor(Math.random() * charactersLength));
+       }
+      return password;
+    }
+
+    console.log(makePassword(length));
+  }
+
+  // Generate password without lowercase or uppercase characters
+  else if (special == true && numerical == true && lowercase == false && uppercase == false) {
+    function makePassword(length) {
+       let password           = '';
+       let charactersLength = noLowercaseUppercaseCharacters.length;
+       for ( let i = 0; i < length; i++ ) {
+          password += noLowercaseUppercaseCharacters.charAt(Math.floor(Math.random() * charactersLength));
+       }
+      return password;
+    }
+
+    console.log(makePassword(length));
+  }
+
+  // Generate password with just special characters
+  else if (special == true && numerical == false && lowercase == false && uppercase == false) {
+    function makePassword(length) {
+       let password           = '';
+       let charactersLength = justSpecialCharacters.length;
+       for ( let i = 0; i < length; i++ ) {
+          password += justSpecialCharacters.charAt(Math.floor(Math.random() * charactersLength));
+       }
+      return password;
+    }
+
+    console.log(makePassword(length));
+  }
+
+  // Generate password with just numerical characters
+  else if (special == false && numerical == true && lowercase == false && uppercase == false) {
+    function makePassword(length) {
+       let password           = '';
+       let charactersLength = justNumericalCharacters.length;
+       for ( let i = 0; i < length; i++ ) {
+          password += justNumericalCharacters.charAt(Math.floor(Math.random() * charactersLength));
+       }
+      return password;
+    }
+
+    console.log(makePassword(length));
+  }
+
+  // Generate password with just lowercase characters
+  else if (special == false && numerical == false && lowercase == true && uppercase == false) {
+    function makePassword(length) {
+       let password           = '';
+       let charactersLength = justLowercaseCharacters.length;
+       for ( let i = 0; i < length; i++ ) {
+          password += justLowercaseCharacters.charAt(Math.floor(Math.random() * charactersLength));
+       }
+      return password;
+    }
+
+    console.log(makePassword(length));
+  }
+
+  // Generate password with just uppercase characters
+  else if (special == false && numerical == false && lowercase == false && uppercase == true) {
+    function makePassword(length) {
+       let password           = '';
+       let charactersLength = justUppercaseCharacters.length;
+       for ( let i = 0; i < length; i++ ) {
+          password += justUppercaseCharacters.charAt(Math.floor(Math.random() * charactersLength));
        }
       return password;
     }

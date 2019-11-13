2c0b39b0d5f60304b03d1be36fd2a45920a7b318
Ajschlander <Aschlander21@gmail.com>
2019-11-13 13:19:56 -0600

    Created new folder and file for javascript. Added event listeners for generate and copy buttons

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+// Create global variables
+let password = '';
+
+// Add event listener to generate button
+document.getElementById('generate').addEventListener("click", function() {
+  const length = document.getElementById('legnth-input').value;
+});
+
+// Add event listener to copy button
+document.getElementById('copy').addEventListener("click", function(){
+
+});
